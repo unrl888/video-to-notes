38b4ed14af501268987301bbd87703fea3f1b613
unrl888 <187846148+unrl888@users.noreply.github.com>
2026-05-14 19:27:59 +0200

    feat: add saver and subtitles

---
+++ saver.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class Saver(ABC):
+
+    @abstractmethod
+    def save(self, text: str):
+        pass
+
+
+class SaverTXT(Saver):
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+
+    def save(self, text: str):
+        with open(f"{self.file_name}.txt", mode="w", encoding="utf-8") as file:
+            file.write(text)
