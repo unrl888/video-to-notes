8f8e73a000a3578636933eeb86174be23ff066b9
unrl888 <187846148+unrl888@users.noreply.github.com>
2026-05-14 21:08:10 +0200

    feat(ollama) add ollama

---
+++ core/analyzer/llm/ollama.py
@@ -0,0 +1,11 @@
+from base import LLM
+import ollama
+
+
+class OllamaLLM(LLM):
+    def __init__(self, model: str = "qwen2.5:7b"):
+        self.model = model
+
+    def complete(self, prompt: str) -> str:
+        response = ollama.generate(self.model, prompt)
+        return response['response']
